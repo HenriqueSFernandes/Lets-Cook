584bece4b80ff1c2e6a29884c0f624aab791b828
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-03-24 11:16:40 +0000

    Added login page with email + google auth support.
    I am using the builtin login page UI, maybe we should redesign the login page in the future.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:lets_cook/MainPages/HomePage.dart';
 import 'package:lets_cook/MainPages/NewProductPage.dart';
 import 'package:lets_cook/MainPages/ProfilePage.dart';
 import 'package:lets_cook/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:lets_cook/MainPages/LoginPage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -45,54 +47,63 @@ class _MainAppState extends State<MainApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
       ),
       title: "Let's Cook",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Let's Cook"),
-          elevation: 4,
-        ),
-        bottomNavigationBar: NavigationBar(
-          destinations: const [
-            NavigationDestination(
-              icon: Icon(Icons.home, size: 30),
-              label: "Home",
+      initialRoute:
+          FirebaseAuth.instance.currentUser == null ? "/sign-in" : "/home",
+      routes: {
+        "/sign-in": (context) {
+          return const LoginPage();
+        },
+        "/home": (context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text("Let's Cook"),
+              elevation: 4,
             ),
-            NavigationDestination(
-              icon: Icon(Icons.add, size: 30),
-              label: "Add",
-            ),
-            NavigationDestination(
-              icon: Icon(Icons.person, size: 30),
-              label: "Profile",
-            ),
-          ],
-          labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-          selectedIndex: currentPageIndex,
-          onDestinationSelected: (int index) {
-            setState(() {
-              currentPageIndex = index;
-              _pageController.animateToPage(
-                index,
-                duration: const Duration(milliseconds: 300),
+            bottomNavigationBar: NavigationBar(
+              destinations: const [
+                NavigationDestination(
+                  icon: Icon(Icons.home, size: 30),
+                  label: "Home",
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.add, size: 30),
+                  label: "Add",
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.person, size: 30),
+                  label: "Profile",
+                ),
+              ],
+              labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+              selectedIndex: currentPageIndex,
+              onDestinationSelected: (int index) {
+                setState(() {
+                  currentPageIndex = index;
+                  _pageController.animateToPage(
+                    index,
+                    duration: const Duration(milliseconds: 300),
 // Adjust duration as needed
-                curve: Curves.ease,
-              );
-            });
-          },
-        ),
-        body: PageView(
-          controller: _pageController,
-          onPageChanged: (index) {
-            setState(() {
-              currentPageIndex = index;
-            });
-          },
-          children: const [
-            HomePage(key: PageStorageKey('HomePage')),
-            NewProductPage(key: PageStorageKey('NewProductPage')),
-            ProfilePage(key: PageStorageKey('ProfilePage')),
-          ],
-        ),
-      ),
+                    curve: Curves.ease,
+                  );
+                });
+              },
+            ),
+            body: PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+                setState(() {
+                  currentPageIndex = index;
+                });
+              },
+              children: const [
+                HomePage(key: PageStorageKey('HomePage')),
+                NewProductPage(key: PageStorageKey('NewProductPage')),
+                ProfilePage(key: PageStorageKey('ProfilePage')),
+              ],
+            ),
+          );
+        },
+      },
     );
   }
 }
