97e641f50d18f02e9a1e756a2e1380a7b289d83d
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-04-11 12:03:25 +0100

    Changed the orientation so that the app stays in portrait mode

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lets_cook/MainPages/HomePage.dart';
 import 'package:lets_cook/MainPages/LoginPage.dart';
@@ -10,6 +11,7 @@ import 'package:lets_cook/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
