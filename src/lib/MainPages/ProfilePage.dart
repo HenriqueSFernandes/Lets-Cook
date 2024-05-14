e61835175b750f9f90b25ae5e1fa262b7e383df5
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-05-14 13:02:30 +0100

    Fixed an issue where wrong meals were being displayed on the user profile

--- src/lib/MainPages/ProfilePage.dart
+++ src/lib/MainPages/ProfilePage.dart
@@ -16,21 +16,12 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  String bio = "";
-
-  Future<String> getBio() async {
-    final userRef =
-        FirebaseFirestore.instance.collection("users").doc(widget.userID);
-    final snapshot = await userRef.get();
-    final bio = snapshot.data()!["more_about_yourself"];
-    return bio;
-  }
 
   Future<List<Product>> getProducts() async {
     List<Product> products = [];
     final dishesRef = FirebaseFirestore.instance.collection("dishes");
     final userDishesRef = dishesRef.where('userid',
-        isEqualTo: FirebaseAuth.instance.currentUser!.uid);
+        isEqualTo: widget.userID);
     await userDishesRef.get().then((doc) {
       for (var value in doc.docs) {
         products.add(Product(
@@ -46,14 +37,6 @@ class _ProfilePageState extends State<ProfilePage> {
       }
     });
     return products;
-  }
-
-  Future<String> getProfilePictureURL() async {
-    String url = "";
-    final userRef =
-        FirebaseFirestore.instance.collection("users").doc(widget.userID);
-    await userRef.get().then((value) => url = value['image_url']);
-    return url;
   }
 
   Future<Map<String, String>> getUserInfo() async {
@@ -83,111 +66,133 @@ class _ProfilePageState extends State<ProfilePage> {
               List<Product> products = snapshot.data![1] as List<Product>;
               Map<String, String> info =
                   snapshot.data![0] as Map<String, String>;
-              return ListView(
+              return Stack(
                 children: [
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 20),
-                    height: MediaQuery.of(context).size.height / 3,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(info['image_url']!),
-                        fit: BoxFit.fitWidth,
-                      ),
-                    ),
-                  ),
-                  Text(
-                    info['name']!,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 22,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                  ListView(
                     children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(7),
-                        child: Container(
-                          width: 30,
-                          height: 30,
-                          color: Theme.of(context).primaryColor,
-                          child: const Icon(
-                            Icons.star,
-                            color: Colors.white,
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 20),
+                        height: MediaQuery.of(context).size.height / 3,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage(info['image_url']!),
+                            fit: BoxFit.fitWidth,
                           ),
                         ),
                       ),
-                      const SizedBox(width: 5),
                       Text(
-                        "4.8 / 5.0",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Theme.of(context).primaryColor,
-                          fontWeight: FontWeight.bold,
+                        info['name']!,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 22,
                         ),
                       ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(7),
+                            child: Container(
+                              width: 30,
+                              height: 30,
+                              color: Theme.of(context).primaryColor,
+                              child: const Icon(
+                                Icons.star,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 5),
+                          Text(
+                            "4.8 / 5.0",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Theme.of(context).primaryColor,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Container(
+                        margin: const EdgeInsets.only(left: 30, right: 30),
+                        child: Text(info['bio']!),
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        "Available meals:",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 22,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Column(
+                        children: products,
+                      ),
+                      Center(
+                        child: Container(
+                          margin: const EdgeInsets.only(bottom: 10),
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              if (isCurrentUser) {
+                                await FirebaseAuth.instance.signOut();
+                                Navigator.pushNamed(context, '/sign-in');
+                              } else {
+                                showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: const Text(
+                                            "Feature not implemented!"),
+                                        content: const Text(
+                                            "The report system will be available as soon as possible."),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () =>
+                                                Navigator.of(context).pop(),
+                                            child: const Text("Ok"),
+                                          ),
+                                        ],
+                                      );
+                                    });
+                              }
+                            },
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.red),
+                            ),
+                            child: Text(
+                              isCurrentUser ? "Sign-out" : "Report",
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
                     ],
                   ),
-                  const SizedBox(height: 20),
-                  Container(
-                    margin: const EdgeInsets.only(left: 30, right: 30),
-                    child: Text(info['bio']!),
-                  ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    "Available meals:",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 22,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Column(
-                    children: products,
-                  ),
-                  Center(
-                    child: Container(
-                      margin: const EdgeInsets.only(bottom: 10),
-                      child: ElevatedButton(
-                        onPressed: () async {
-                          if (isCurrentUser) {
-                            await FirebaseAuth.instance.signOut();
-                            Navigator.pushNamed(context, '/sign-in');
-                          } else {
-                            showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return AlertDialog(
-                                    title:
-                                        const Text("Feature not implemented!"),
-                                    content: const Text(
-                                        "The report system will be available as soon as possible."),
-                                    actions: [
-                                      TextButton(
-                                        onPressed: () =>
-                                            Navigator.of(context).pop(),
-                                        child: const Text("Ok"),
-                                      ),
-                                    ],
-                                  );
-                                });
-                          }
-                        },
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all(Colors.red),
-                        ),
-                        child: Text(
-                          isCurrentUser ? "Sign-out" : "Report",
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                          ),
+                  isCurrentUser ? const SizedBox() : Positioned(
+                    top: 40,
+                    left: 20,
+                    child: ElevatedButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      style: ElevatedButton.styleFrom(
+                        shape: const CircleBorder(),
+                        padding: const EdgeInsets.all(5),
+                        backgroundColor: Theme.of(context).primaryColor,
+                      ),
+                      child: const Center(
+                        child: Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
                         ),
                       ),
                     ),
-                  )
+                  ),
                 ],
               );
             }
