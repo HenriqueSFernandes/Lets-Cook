d970e4812de1b714dce83256361afdc3402c4a2d
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-05-22 11:06:48 +0100

    Fixed an issue where the messages page didn't load when there were no messages

--- src/lib/MainPages/ChatListPage.dart
+++ src/lib/MainPages/ChatListPage.dart
@@ -19,83 +19,86 @@ class _ChatListPageState extends State<ChatListPage> {
 
   @override
   Widget build(BuildContext context) {
-    final chats = widget.rooms.map(
-      (e) {
-        String receiverID = e["receiverID"];
-        String mealID = e["mealID"];
-        final userRef =
-            FirebaseFirestore.instance.collection("users").doc(receiverID);
-        final mealRef =
-            FirebaseFirestore.instance.collection("dishes").doc(mealID);
-        return FutureBuilder(
-          future: Future.wait([userRef.get(), mealRef.get()]),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              String userName = snapshot.data![0]["name"];
-              String mealName = snapshot.data![1]["mealname"];
-              final String imageURL = snapshot.data![0]["image_url"];
-              return Padding(
-                padding: const EdgeInsets.only(bottom: 20),
-                child: GestureDetector(
-                  child: Row(
-                    children: [
-                      Container(
-                        width: 50,
-                        height: 50,
-                        margin: const EdgeInsets.only(
-                          left: 20,
-                          right: 20,
-                        ),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Colors.grey,
-                              blurRadius: 4,
-                              offset: Offset(2, 2),
-                            )
-                          ],
-                          border: Border.all(
-                            color: Theme.of(context).primaryColor,
-                            width: 2,
+    List<Widget> chats = [];
+    if (widget.rooms.isNotEmpty) {
+      chats = widget.rooms.map(
+        (e) {
+          String receiverID = e["receiverID"];
+          String mealID = e["mealID"];
+          final userRef =
+              FirebaseFirestore.instance.collection("users").doc(receiverID);
+          final mealRef =
+              FirebaseFirestore.instance.collection("dishes").doc(mealID);
+          return FutureBuilder(
+            future: Future.wait([userRef.get(), mealRef.get()]),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                String userName = snapshot.data![0]["name"];
+                String mealName = snapshot.data![1]["mealname"];
+                final String imageURL = snapshot.data![0]["image_url"];
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: GestureDetector(
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          margin: const EdgeInsets.only(
+                            left: 20,
+                            right: 20,
                           ),
-                          image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: imageURL == ""
-                                ? const AssetImage(
-                                    "lib/resources/default_userimage.png")
-                                : NetworkImage(imageURL) as ImageProvider,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.grey,
+                                blurRadius: 4,
+                                offset: Offset(2, 2),
+                              )
+                            ],
+                            border: Border.all(
+                              color: Theme.of(context).primaryColor,
+                              width: 2,
+                            ),
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: imageURL == ""
+                                  ? const AssetImage(
+                                      "lib/resources/default_userimage.png")
+                                  : NetworkImage(imageURL) as ImageProvider,
+                            ),
                           ),
                         ),
-                      ),
-                      Text(
-                        "$userName - $mealName",
-                        style: const TextStyle(
-                          fontSize: 20,
+                        Text(
+                          "$userName - $mealName",
+                          style: const TextStyle(
+                            fontSize: 20,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ChatPage(
+                            receiverID: e["receiverID"],
+                            mealID: e["mealID"],
+                          ),
+                        ),
+                      );
+                    },
                   ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ChatPage(
-                          receiverID: e["receiverID"],
-                          mealID: e["mealID"],
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              );
-            } else {
-              return const CircularProgressIndicator();
-            }
-          },
-        );
-      },
-    ).toList();
+                );
+              } else {
+                return const CircularProgressIndicator();
+              }
+            },
+          );
+        },
+      ).toList();
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text("Chats"),
